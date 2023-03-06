cfa32e487c05766ac1c59125bf4c2bfdc12cf480
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-06 14:34:14 -0700

    completed assignment

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,26 +1,52 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
 
-            int a = 17;
-            int b = 4;
+            var b = 4;
 
-            int quotient = a / b;
-            int remainder = a % b;
+            var sum = a + b;
 
-            if (a == 17 && b == 4)
-            {
-                Console.WriteLine("17 / 4 is 4 remainder 1");
-            }
+            var diff = a - b;
 
+            var multi = a * b;
 
+            var divi = a / b;
 
+            var remainder = a % b;
 
+            Console.WriteLine($"{a} plus {b} = {sum}.");
 
+            Console.WriteLine($"{a} subtract {b} = {diff}.");
+
+            Console.WriteLine($"{a} multiplied by {b} = {multi}.");
+
+            Console.WriteLine($"{a} divided by {b} = {divi}.");
+
+            Console.WriteLine($"The remainder of {a} divided by {b} is {a % b}");
+
+            //Exercise 2 Area of a Circle
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine("Area of Circle");
+            
+            Console.WriteLine($"The area of the circle with the radius of {radius} is {area}");
+
+            // Though Expermiment
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+
+            Console.WriteLine(k); //16
         }
     }
 }
